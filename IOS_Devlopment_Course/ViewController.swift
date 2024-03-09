ce90404cd0fe7fbe5908d8a63b0e0ec04175d238
hakraoul <haknjr11@gmail.com>
2024-03-09 16:45:00 +0700

    close #2 add button to show Hello World message

--- IOS_Devlopment_Course/ViewController.swift
+++ IOS_Devlopment_Course/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func showMessage (sender: UIButton) {
+        var alertController = UIAlertController(title: "Welcome to Swift", message: "Hello world", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
